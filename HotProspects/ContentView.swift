12e4d0cd2975c9e918bf21d3f6d5012206813a09
Peter Hartnett <peter.hartnett@gmail.com>
2022-07-19 10:18:50 -0400

    small updates and pushing images

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -11,6 +11,8 @@
 //2) ☑️Use JSON and the documents directory for saving and loading our user data.
 //3) ☑️Use a confirmation dialog to customize the way users are sorted in each tab – by name or by most recent.
 
+//TODO: Highlight the parts of the code where the challenges exist.
+
 import SwiftUI
 
 
